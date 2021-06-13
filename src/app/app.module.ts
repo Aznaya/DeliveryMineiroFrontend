6a67444cd0e7cf1426fb904bc2522978b36a54e8
Jonas Antunes <jonasantunes@gec.inatel.br>
2021-06-12 22:03:50 -0300

    Mexendo com as paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { InitialPageComponent } from './pages/initial-page/initial-page.component';
 import { RoutingModule } from './app.router';
-import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 
@@ -19,8 +17,6 @@ import { Error404PageComponent } from './pages/error404-page/error404-page.compo
     AppComponent,
     LoginPageComponent,
     HomePageComponent,
-    InitialPageComponent,
-    HeaderComponent,
     Error404PageComponent
   ],
   imports: [
