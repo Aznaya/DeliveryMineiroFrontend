41924523c17c5daf71b0fd0cca107e374f050819
Jonas Antunes <jonasantunes@gec.inatel.br>
2021-06-19 23:54:24 -0300

    Tela de pedido

--- src/app/pages/pedido-page/pedido-page.component.ts
+++ src/app/pages/pedido-page/pedido-page.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pedido-page.component.css']
 })
 export class PedidoPageComponent implements OnInit {
+  public dinheiro: boolean = false;
+  public cartao: boolean = false;
+  public troco: Number = 0;
+  public bandeira: String = '';
+  public bandeiras = [
+    'Visa',
+    'Mastercard',
+    'American Express',
+    'Elo',
+    'Hipercard',
+    'Dinners Club',
+    'Sorocred'
+  ];
+  public endereco: String = 'Anor Londor, 115, Atrás dos Gigantes, se chegar no salão principal passou';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  tipoPagamento(ehDinheiro: boolean): void {
+    if (ehDinheiro) {
+      this.dinheiro = true;
+      this.cartao = false;
+    } else {
+      this.dinheiro = false;
+      this.cartao = true;
+    }
+  }
+
+  realizarPedido(): void {
+    console.log('Realizando Pedido');
+    console.log('O pagamento será em ');
+    if (this.dinheiro) {
+      console.log('Dinheiro com troco de ', this.troco)
+    }
+    if (this.cartao) {
+      console.log('Cartão com bandeira da ', this.bandeira)
+    }
+    console.log('No endereço: ', this.endereco)
+  }
 }
